0c5064d7ccae73a25d6e44d26a26307a794549fc
FatemaElzahraaGamalAbdElAziz <42900191+FatemaElzahraaGamalAbdElAziz@users.noreply.github.com>
2018-10-25 21:54:07 +0200

    Add files via upload

---
+++ EdgeMagnit.m
@@ -0,0 +1,22 @@
+function [Result] = EdgeMagnit(I)
+  X = Sobel('H');
+  firstImage = linearfilter(I , X , "absolute");
+  Y = Sobel('V');
+  secondImage = linearfilter(I , Y , "absolute");
+  Z = firstImage + secondImage;
+  
+  for i = 1 : rows(Z)
+    for j = 1 :columns(Z)
+      
+      if(Z(i,j)>255)
+        Z(i,j)=255;
+      elseif (Z(i,j)<0)
+        Z(i,j)=0;
+      endif 
+      
+    endfor
+    endfor
+  
+  Result = Z;
+  
+endfunction
--- Sobel.m
+++ Sobel.m
@@ -1,4 +1,4 @@
-function Sobel(Mask)
+function SMask = Sobel(Mask)
   h = [-1, -2,-1;
            0,0,0;
            1, 2,1];
@@ -6,25 +6,8 @@ function Sobel(Mask)
         -2,0,2;
         -1, 0,1];
   if(Mask=='H')
-    disp(h);
-    #call karim's fn given h
+    SMask = h;
   elseif(Mask=='V')
-    disp(v);
-  else
-    disp(h);
-    disp(v);
+    SMask = v;
   endif
-  Cutoff(v);
-endfunction
- 
-function Cutoff(Image)
-  for i= 1:rows(Image)
-    for j=1:columns(Image)
-      if(Image(i,j,:) > 255)
-        Image(i,j,:)=255;
-    #{  elseif(Image(i,j,:) < 0)
-         Image(i,j,:)=0; #}
-      endif
-    endfor
-  endfor
 endfunction
